1456767553c4bd9901504ca1362df25bf6bccb4a
Ajay <102792088+ajay-dv8@users.noreply.github.com>
2023-01-28 05:32:20 -0800

    fixing footer issues

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './footer.css'
+import { useNavigate } from 'react-router-dom'
 import { 
-    LinkedIn, Phone, Telegram, WhatsApp, GitHub 
+    LinkedIn, Phone, Telegram, GitHub 
 } from '@mui/icons-material'
 
 const Footer = () => {
+
+  const navigate = useNavigate();
+  
+  function toPortfolio(){
+    navigate("https://dv8portfolio.netlify.app/");
+  }
+
   return (
+    <div className='footer_container'>
     <div className='footer'>
         <div className='ac'>
             <h3 className='fTitle'>Ac</h3>
@@ -20,6 +29,17 @@ const Footer = () => {
             <p>Accra / Koforidua (GH)</p>
         </div>
 
+        <div className='abt_dev'>
+            <h3 className='fTitle'>Developer</h3>
+            <br/>
+            <div className='dev_list'>
+                <span>Ajay Dives</span>
+                <span onClick={toPortfolio}>Portfolio</span>
+                <span>Contact</span>
+                <span>Team</span>
+            </div>
+        </div>
+
         <div className='socials'>
             <h3 className='fTitle'>Socials</h3>
 
@@ -28,23 +48,33 @@ const Footer = () => {
                 <a href='#'>
                     <Telegram className='icon muiTelegram'/>
                 </a>
-                <a href='#'>
-                    <WhatsApp className='icon muiWhatsApp'/>
-                </a>
-                <a href='#'>
+                <a href='https://github.com/ajay-dv8'>
                     <GitHub className='icon muiGitHub'/>
                 </a>
                 <a href='#'>
                     <LinkedIn className='icon muiLinkedIn'/>
                 </a>
-                <a href='#'>
+                <a href='tel:+233550414170'>
                     <Phone className='icon muiPhone'/>
                 </a>
             </div>
         </div>
-
-        <div className='about'></div>
     
+    </div>
+    <hr style={{borderColor:'black'}}/>
+        <div className='footer_bottom'>
+            
+            <div className='f_contact_info'>
+                <span>email: ajaydives3@gmail.com | </span>
+                <span>phone: +2335 5041 4170</span>
+            </div>
+            <div>
+                <span className='copyright_info'>
+                2023 copyright | dv8 | Ajay Dives.  
+                All rights reserved.
+                </span>
+            </div>
+        </div>
     </div>
   )
 }
